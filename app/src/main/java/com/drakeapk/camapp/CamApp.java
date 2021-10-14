7d9bf3a1bad70fc5b02110f0508afa94da586740
Drake343 <87871563+Drake343@users.noreply.github.com>
2021-10-14 16:40:13 +0530

    camera app

---
+++ app/src/main/java/com/drakeapk/camapp/CamApp.java
@@ -0,0 +1,3 @@
+package com.drakeapk.camapp;
+
+import android.app.Activity;
---
+++ app/src/main/java/com/drakeapk/camapp/MainActivity.kt
@@ -0,0 +1,65 @@
+package com.drakeapk.camapp
+
+import android.Manifest.permission.CAMERA
+import android.Manifest.permission.READ_EXTERNAL_STORAGE
+import android.app.Activity
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
+import android.view.View
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
+import java.io.File
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.*
+
+private const val REQUEST_IMAGE_CAPTURE = 1
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var cameraBtn : Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        cameraBtn = findViewById(R.id.btnTakePicture)
+        cameraBtn.setOnClickListener {
+            dispatchTakePictureIntent()
+        }
+
+    }
+
+    private fun dispatchTakePictureIntent() {
+        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        try {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+        } catch (e: ActivityNotFoundException) {
+            // display error state to the user
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            val imageBitmap = data?.extras?.get("data") as Bitmap
+            findViewById<ImageView>(R.id.imageView).setImageBitmap(imageBitmap)
+        }
+        else{
+            super.onActivityResult(requestCode, resultCode, data)
+        }
+    }
+
+}
